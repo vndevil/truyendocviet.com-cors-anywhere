e4e6f6dfa620aff29f6042a70eb25617d2a7960e
VnDevil <vndevil@gmail.com>
2023-08-22 14:45:15 +0700

    update

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   //originWhitelist: originWhitelist,
-  originWhitelist: ['http://localhost:1991', ['https://truyendocviet.com']],
+  originWhitelist: ['https://truyendocviet.com', 'http://localhost'],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
